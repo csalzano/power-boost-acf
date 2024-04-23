4e92defcf853a267677eee2a6f50ebabbd77eba5
Corey Salzano <938411+csalzano@users.noreply.github.com>
2024-04-23 11:05:59 -0400

    Bug fix. Save each field with the proper parent post ID when saving Local Field Groups to the database.

--- includes/class-acfpb-tool-local-groups.php
+++ includes/class-acfpb-tool-local-groups.php
@@ -157,7 +157,13 @@ if ( class_exists( 'ACF_Admin_Tool' ) ) {
 
 			// Save each field group to the database.
 			foreach ( $exportable as $field_group ) {
-				acf_update_field_group( $field_group );
+				$field_group = acf_update_field_group( $field_group );
+				// Save each field.
+				foreach ( $field_group['fields'] as $field ) {
+					$field           = acf_get_field( $field['key'] );
+					$field['parent'] = $field_group['ID'];
+					acf_update_field( $field );
+				}
 			}
 		}
 	}
